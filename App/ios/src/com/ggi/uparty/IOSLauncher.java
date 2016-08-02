2b006966bb3b02d87692b98dc737765eec720612
Emmett <GoodGameIndustries@Emmetts-MacBook-Pro.local>
2016-08-02 04:59:42 -0400

    8/2/2016 IOS working

--- App/ios/src/com/ggi/uparty/IOSLauncher.java
+++ App/ios/src/com/ggi/uparty/IOSLauncher.java
@@ -1,5 +1,12 @@
 package com.ggi.uparty;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.robovm.apple.corelocation.CLLocation;
+import org.robovm.apple.corelocation.CLLocationManager;
+import org.robovm.apple.corelocation.CLLocationManagerDelegateAdapter;
+import org.robovm.apple.foundation.NSArray;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 
@@ -8,8 +15,26 @@ import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import com.ggi.uparty.uParty;
 
 public class IOSLauncher extends IOSApplication.Delegate implements NativeController {
-    @Override
+    public CLLocationManager c;
+    public List<CLLocation> locationMeasurements = new ArrayList<CLLocation>();
+	
+	@Override
     protected IOSApplication createApplication() {
+		
+		c = new CLLocationManager();
+		if(c.isLocationServicesEnabled()){
+		c.setDelegate(new CLLocationManagerDelegateAdapter(){
+			
+			public void didUpdateLocations(CLLocationManager manager, NSArray<CLLocation> locations){
+				CLLocation newLocation = locations.last();
+				locationMeasurements.add(newLocation);
+			}
+			
+		});
+		c.setDesiredAccuracy(10);
+		c.requestWhenInUseAuthorization();
+		c.startUpdatingLocation();}
+		
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
         return new IOSApplication(new uParty(this), config);
     }
@@ -23,12 +48,12 @@ public class IOSLauncher extends IOSApplication.Delegate implements NativeContro
 	@Override
 	public float getLong() {
 		// TODO Auto-generated method stub
-		return 0;
+		return (float) locationMeasurements.get(locationMeasurements.size()-1).getCoordinate().getLongitude();
 	}
 
 	@Override
 	public float getLat() {
 		// TODO Auto-generated method stub
-		return 0;
+		return (float) locationMeasurements.get(locationMeasurements.size()-1).getCoordinate().getLatitude();
 	}
 }
