782412770227a490e3be2a190bbead16c770a4e5
Emmett <goodgameindustries@gmail.com>
2017-01-18 10:24:21 -0600

    more debugging

--- LoadBalance/src/com/ggi/uparty/loadbalance/UPLoad.java
+++ LoadBalance/src/com/ggi/uparty/loadbalance/UPLoad.java
@@ -116,6 +116,7 @@ public class UPLoad extends JFrame{
 							System.out.println("Selecting Server to send:");
 							System.out.println(servs.get(0).c.getRemoteAddressTCP().getHostString());
 							String split[] = servs.get(0).c.getRemoteAddressTCP().getHostString().split(".");
+							System.out.println(split.length);
 							auth.servIP="ec2-"+split[0]+"-"+split[1]+"-"+split[2]+"-"+split[3]+".us-west-2.compute.amazonaws.com";
 							System.out.println("Converting to DNS");
 							System.out.println(auth.servIP);
