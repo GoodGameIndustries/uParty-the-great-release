ce00202b7e96d6fbcbd40eb6971d5a282e4cb93b
Emmett <goodgameindustries@gmail.com>
2017-02-22 10:21:18 -0600

    send group fix

--- Server/src/com/ggi/uparty/server/UPServer.java
+++ Server/src/com/ggi/uparty/server/UPServer.java
@@ -480,6 +480,7 @@ public class UPServer extends JFrame {
 				for (int i = 0; i < a.groups.size(); i++) {
 					Group g = a.groups.get(i);
 					boolean sent = false;
+					if(g.name.length()>0){
 					Group g2 = loadGroup(g.name.replace(" ", "") + g.owner.replace(".", "_").replace("@", "_"));
 					if (g2 != null) {
 
@@ -497,6 +498,7 @@ public class UPServer extends JFrame {
 					if (!sent) {
 						a.groups.remove(i);
 					}
+				}
 				}
 				saveAccount(a);
 
